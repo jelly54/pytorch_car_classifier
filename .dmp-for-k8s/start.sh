f3368f6ed3855d7479f65252e48627d4e60c6dda
张国栋 <guodongzhang@linkdoc.com>
2019-12-18 18:49:21 +0800

    修改dockerfile 更换基础镜像

--- .dmp-for-k8s/start.sh
+++ .dmp-for-k8s/start.sh
@@ -4,8 +4,6 @@ SERVICE=$1
 
 nohup python /home/work/code/pytorch_car_classifier/classifier_train.py >> /home/work/logs/pytorch_car_classifier/train.log
 
-# 启动nginx 
-nginx
 
 #由于k8s中要求镜像启动脚本必须挂在前台不退出，这里用tail一个文件的方式解决。
-tail -f /home/work/var/pytorch_car_classifier/nginx/access.log
+tail -f /home/work/logs/pytorch_car_classifier/train.log
